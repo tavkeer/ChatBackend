3aa3651f2620e6b859d938829f5ff46c8a90ebdf
tavkeer <shahtavkeer@gmail.com>
2024-01-29 14:10:29 +0530

    update

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"golang.org/x/net/websocket"
 )
@@ -35,6 +36,15 @@ func (s *Server) readLoop(ws *websocket.Conn) {
 	}
 }
 
+func (s *Server) handleWSOrderbook(ws *websocket.Conn) {
+	fmt.Println("new incomming connection from the client to orderbook feed :", ws.RemoteAddr())
+	for {
+		payload := fmt.Sprintf("orderbook data:= %d\n", time.Now().UnixNano())
+		ws.Write([]byte(payload))
+		time.Sleep(time.Second * 2)
+	}
+}
+
 func (s *Server) handleWS(ws *websocket.Conn) {
 	fmt.Println("New incomming connection from client", ws.RemoteAddr())
 	s.conns[ws] = true
@@ -54,5 +64,6 @@ func (s *Server) Broadcast(b []byte) {
 func main() {
 	server := NewServer()
 	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.Handle("/feed", websocket.Handler(server.handleWSOrderbook))
 	http.ListenAndServe(":3000", nil)
 }
