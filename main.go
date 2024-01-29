a3c4dc4f5a5926e214cea6472361e9fcecbc69e8
tavkeer <shahtavkeer@gmail.com>
2024-01-29 14:00:08 +0530

    working for single user broadcast

--- main.go
+++ main.go
@@ -1,11 +1,58 @@
 package main
 
-import "golang.org/x/net/websocket"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
 
 type Server struct {
 	conns map[*websocket.Conn]bool
 }
 
-func main() {
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
 
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read err:", err)
+			continue
+		}
+		msg := buf[:n]
+		s.Broadcast(msg)
+
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("New incomming connection from client", ws.RemoteAddr())
+	s.conns[ws] = true
+	s.readLoop(ws)
+}
+
+func (s *Server) Broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error:", err)
+			}
+		}(ws)
+	}
+}
+
+func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.ListenAndServe(":3000", nil)
 }
